173f2ee1d828a9de4b72a04e85748e755ccbe834
mazhaojun <mazhaojun@offcn.com>
2019-03-20 10:24:21 +0800

    增加第12题

--- problem_12.js
+++ problem_12.js
@@ -9,5 +9,5 @@ var intToRoman = function(num) {
         Xparts = ['', 'X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC'],
         Iparts = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'];
 
-  return Mparts[parseInt(num / 1000)] + Cparts[parseInt((num % 1000) / 100)] + Xparts[parseInt((num % 100) / 10)] + Iparts[parseInt(num % 10)];
+  return Mparts[Math.floor(num / 1000)] + Cparts[Math.floor((num % 1000) / 100)] + Xparts[Math.floor((num % 100) / 10)] + Iparts[Math.floor(num % 10)];
 };
